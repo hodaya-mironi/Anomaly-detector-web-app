b81db225301d8fc745a37410e55e1166cb0ba0d7
Hodaya Mironi <hodayamironi@gmail.com>
2021-05-06 18:30:30 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ModelsListComponent } from './models-list/models-list.component';
+import { GraphsComponent } from './graphs/graphs.component';
+import { DropOffComponent } from './drop-off/drop-off.component';
+import { DataTableComponent } from './data-table/data-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelsListComponent,
+    GraphsComponent,
+    DropOffComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule
